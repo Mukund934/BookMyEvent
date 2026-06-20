a2622eb771d76136f688c63b5fb09d08242c1b7e
Mukund Thakur <mukund.th04@gmail.com>
2026-06-20 13:22:00 +0530

    integrate mongoDB with backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (_, res) => {
+	res.json({
+		success: true,
+		message: "BookMyEvent API Running"
+	});
+});
+
 app.use("/api", routes);
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,27 @@
 import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const PORT = 5000;
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+	console.error("MONGO_URI is not defined in .env");
+	process.exit(1);
+}
+
+mongoose
+	.connect(MONGO_URI)
+	.then(() => {
+		console.log("MongoDB Connected Successfully");
+
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error("MongoDB Connection Error:", error);
+		process.exit(1);
+	});
