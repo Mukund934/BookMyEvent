606661b85c6f75db7360bd732dace984753b3a9f
Mukund Thakur <mukund.th04@gmail.com>
2026-06-23 20:25:49 +0530

    backend deployed

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-	"http://localhost:5000/api";
+	"https://bookmyevent-backend-2u7p.onrender.com/api";
 
 const api = axios.create({
 	baseURL: API_BASE_URL,
